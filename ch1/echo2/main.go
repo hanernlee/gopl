4068b095164f43ab6ff615c54ad128db107c67a9
Chris Lee <Chris.Lee@carsales.com.au>
2019-04-10 23:20:34 +1000

    Chapter 1

---
+++ ch1/echo2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for index, arg := range os.Args[0:] {
+		s += sep + arg
+		sep = " "
+		fmt.Println(index)
+		fmt.Println(arg)
+	}
+	fmt.Println(s)
+}
